bedce5570ce62a5c466525b2df22cf8647a6387e
Trevor Whitney <trevorjwhitney@gmail.com>
2013-03-20 15:20:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkinTrack::Application.config.secret_token = 'a5521ec943f0743f57649fd40b738f2be3414a3bd3092b30fbfc964c407e73c760a039bd49b8a12b1db580dfdc58c08cc46581544887c602254cfa0a13c5fe91'
